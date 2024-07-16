260ec5f752a3b843b267aad1c1c14d555c8ba325
Hannes Unterholzner <unterholznerhannes@gmail.com>
2024-07-16 10:37:33 +0100

    backend front end communication working (but slooow)

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import speechmatics
 import sounddevice as sd
+import queue
+import pygame
+import threading
+
 
 API_KEY = "gn9nO1Bjx03leYBlTQfhkWC5hMXr0E2i"
 LANGUAGE = "en"
 CONNECTION_URL = f"wss://eu2.rt.speechmatics.com/v2/{LANGUAGE}"
 DEVICE_INDEX = -1
 CHUNK_SIZE = 1024
-
-
+QUEUE = queue.Queue()
 SAMPLE_RATE = 44_100
+
 class RawInputStreamWrapper:
     def __init__(self, wrapped: sd.RawInputStream):
         self.wrapped: sd.RawInputStream = wrapped
@@ -18,14 +22,12 @@ class RawInputStreamWrapper:
 
 
 class SMTranscribe:
-    def __init__(self) -> None:
-        self.word = "DEFAULT"
-    
     def transcript_handler(self, message):
         if message.get('results', None):
-            self.word = message['results'][0]['alternatives'][0]['content']
-            print(self.word, self.file)
-    
+            for results in message['results']:
+                # print(results['alternatives'][0]['content'])
+                QUEUE.put(results['alternatives'][0]['content'])
+
     def run(self):
         # Define connection parameters
         conn = speechmatics.models.ConnectionSettings(
@@ -34,14 +36,15 @@ class SMTranscribe:
         )
         self.ws = speechmatics.client.WebsocketClient(conn)
         self.ws.add_event_handler(
-            event_name=speechmatics.models.ServerMessageType.AddTranscript,
+            event_name=speechmatics.models.ServerMessageType.AddPartialTranscript,
             event_handler=self.transcript_handler,
         )
 
         transcription_conf = speechmatics.models.TranscriptionConfig(
             language=LANGUAGE,
             enable_partials=True,
-            max_delay=1.6,
+            max_delay=0.7,
+            max_delay_mode="fixed",
             punctuation_overrides={
                 "permitted_marks": [],
             },
@@ -58,5 +61,50 @@ class SMTranscribe:
             ) as stream:
             self.ws.run_synchronously(RawInputStreamWrapper(stream), transcription_conf, settings)
 
-transcriber = SMTranscribe()
-transcriber.run()
+
+def display_text(word, screen, background_color, text_color):
+    screen.fill(background_color)
+    font = pygame.font.Font(None, 26)
+    text_surface = font.render(word, True, text_color)
+    screen.blit(text_surface, (20, 20))
+    pygame.display.flip()
+
+
+def main():
+    pygame.init()
+    background_color = (255, 255, 255)
+    text_color = (0, 0, 0)
+    screen = pygame.display.set_mode([500, 500])
+    screen.fill(background_color)
+    pygame.display.flip()
+
+    transcriber = SMTranscribe()
+    thread = threading.Thread(target=transcriber.run)
+    thread.start()
+
+    # Run until the user asks to quit
+    running = True
+    word = ""
+    while running:
+        # Did the user click the window close button?
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+            try:
+                word = QUEUE.get(block=False)
+            except:
+                word = word
+            else:
+                print(word)
+                display_text(word, screen, background_color, text_color)
+
+
+    # Done! Time to quit.
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
+
+ 
